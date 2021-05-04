cab1ee250afaf681f0abb582a0c75713a0d443c3
MagicAdamNo1 <33387551+MagicAdamNo1@users.noreply.github.com>
2021-05-05 00:54:00 +0800

    example1

---
+++ hello.js
@@ -0,0 +1,10 @@
+/*
+ * @Descripttion:
+ * @version: v0.1
+ * @Author: Elon C
+ * @Date: 2021-05-04 23:41:33
+ * @LastEditors: Elon C
+ * @LastEditTime: 2021-05-04 23:43:22
+ * @FilePath: \Github\learn-github-actions\.github\workflows\hello.js
+ */
+console.log('hello world!');
